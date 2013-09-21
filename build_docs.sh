392002723914aeb2252ed09cbe28e144b20e1983
AntKarlov <ant.karlov@gmail.com>
2013-09-21 12:40:37 +0400

    Обновленная документация.

--- build_docs.sh
+++ build_docs.sh
@@ -1,1 +1,1 @@
-/Library/Developer/SDKs/flex-sdk-3/bin/asdoc -source-path /Users/antonkarlov/Dropbox/Projects/AnthillPublic -doc-sources /Users/antonkarlov/Dropbox/Projects/AnthillPublic -output /Users/antonkarlov/Dropbox/Projects/AnthillPublic/doc
+/Library/Developer/SDKs/flex-sdk-46/bin/asdoc -source-path /Users/antonkarlov/Dropbox/Projects/AnthillPublic -doc-sources /Users/antonkarlov/Dropbox/Projects/AnthillPublic -output /Users/antonkarlov/Dropbox/Projects/AnthillPublic/doc
